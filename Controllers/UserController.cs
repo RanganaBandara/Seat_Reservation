eeae320d6ab25cb1ed9860b025fe3bb3e916d052
Rangana <Rangana@DESKTOP-GC6F9E5>
2024-08-23 03:17:39 +0530

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Seat_Reservation.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserController: ControllerBase
+{
+
+    
+
+}
